df1264328d4fa07b4fc9ef1f6b757bdbdfb55413
IvanVeltz <ivan?veltz@live.fr>
2026-06-18 14:16:45 +0200

    Secure training session creation

--- src/State/TrainingSessionProcessor.php
+++ src/State/TrainingSessionProcessor.php
@@ -6,11 +6,14 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\TrainingSession;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class TrainingSessionProcessor implements ProcessorInterface
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager
+        private readonly EntityManagerInterface $entityManager,
+        private readonly Security $security,
     ) {
     }
 
@@ -18,6 +21,18 @@ class TrainingSessionProcessor implements ProcessorInterface
     {
         if (!$data instanceof TrainingSession) {
             throw new \LogicException('This processor only supports TrainingSession entities.');
+        }
+
+        $user = $this->security->getUser();
+
+        $athlete = $data->getAthlete();
+
+        if ($athlete === null) {
+            throw new AccessDeniedHttpException('Athlete is required.');
+        }
+
+        if ($athlete->getCoach() !== $user) {
+            throw new AccessDeniedHttpException('You cannot create a session for this athlete.');
         }
 
         $now = new \DateTimeImmutable();
