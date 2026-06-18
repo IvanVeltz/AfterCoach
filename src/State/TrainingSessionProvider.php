10cd12b3127031ce1e4e49e8e15a312d34444729
IvanVeltz <ivan?veltz@live.fr>
2026-06-18 14:36:36 +0200

    Fix training session patch provider

--- src/State/TrainingSessionProvider.php
+++ src/State/TrainingSessionProvider.php
@@ -2,16 +2,20 @@
 
 namespace App\State;
 
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Patch;
 use ApiPlatform\State\ProviderInterface;
 use App\Repository\TrainingSessionRepository;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TrainingSessionProvider implements ProviderInterface
 {
     public function __construct(
-        private TrainingSessionRepository $trainingSessionRepository,
-        private Security $security,
+        private readonly TrainingSessionRepository $trainingSessionRepository,
+        private readonly Security $security,
     ) {
     }
 
@@ -19,12 +23,36 @@ class TrainingSessionProvider implements ProviderInterface
     {
         $user = $this->security->getUser();
 
-        return $this->trainingSessionRepository
-            ->createQueryBuilder('ts')
-            ->join('ts.athlete', 'a')
-            ->andWhere('a.coach = :coach')
-            ->setParameter('coach', $user)
-            ->getQuery()
-            ->getResult();
+        if ($operation instanceof GetCollection) {
+            return $this->trainingSessionRepository
+                ->createQueryBuilder('ts')
+                ->join('ts.athlete', 'a')
+                ->andWhere('a.coach = :coach')
+                ->setParameter('coach', $user)
+                ->getQuery()
+                ->getResult();
+        }
+
+        if ($operation instanceof Get || $operation instanceof Patch) {
+            $id = $uriVariables['id'] ?? null;
+
+            $trainingSession = $this->trainingSessionRepository
+                ->createQueryBuilder('ts')
+                ->join('ts.athlete', 'a')
+                ->andWhere('ts.id = :id')
+                ->andWhere('a.coach = :coach')
+                ->setParameter('id', $id)
+                ->setParameter('coach', $user)
+                ->getQuery()
+                ->getOneOrNullResult();
+
+            if ($trainingSession === null) {
+                throw new NotFoundHttpException('Training session not found.');
+            }
+
+            return $trainingSession;
+        }
+
+        return null;
     }
 }
