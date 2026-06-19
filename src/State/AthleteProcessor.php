13f49c58b589cef64f8c53d1aa8395d7b5ff8b14
IvanVeltz <ivan?veltz@live.fr>
2026-06-19 13:03:01 +0200

    Secure athlete soft delete

--- src/State/AthleteProcessor.php
+++ src/State/AthleteProcessor.php
@@ -2,11 +2,13 @@
 
 namespace App\State;
 
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\Athlete;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class AthleteProcessor implements ProcessorInterface
 {
@@ -22,11 +24,26 @@ class AthleteProcessor implements ProcessorInterface
             throw new \LogicException('This processor only supports Athlete entities.');
         }
 
+        $user = $this->security->getUser();
         $now = new \DateTimeImmutable();
 
+        if ($operation instanceof Delete) {
+            if ($data->getCoach() !== $user) {
+                throw new AccessDeniedHttpException('You cannot delete this athlete.');
+            }
+
+            $data->setIsActive(false);
+            $data->setUpdatedAt($now);
+
+            $this->entityManager->flush();
+
+            return $data;
+        }
+
         if ($data->getId() === null) {
-            $data->setCoach($this->security->getUser());
+            $data->setCoach($user);
             $data->setCreatedAt($now);
+            $data->setIsActive(true);
         }
 
         $data->setUpdatedAt($now);
